24400146f54c5e3c656eefef268da8b1657abe40
emanuelf0303 <55004915+emanuelf0303@users.noreply.github.com>
2020-04-24 14:54:01 +0300

    Salary_cal

--- getheader.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	include('config.php');
-	$id = $_POST['edit_id'];
-	$query = mysqli_query($db,'SELECT * FROM headercontent WHERE id = ' . $id);
-	$result = mysqli_fetch_array($query, MYSQLI_ASSOC);
-	echo json_encode($result);
-?>
